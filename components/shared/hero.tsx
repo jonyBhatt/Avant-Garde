daee5b96526ec0bafcd76b1bb195b87ee2156526
Jony <jonybhattacharjee886@gmail.com>
2024-02-09 01:51:23 +0600

    initial and homepage

---
+++ components/shared/hero.tsx
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import { Button } from "../ui/button";
+import styles from "@/components/css/hero.module.css";
+import { cn } from "@/lib/utils";
+
+const Hero = () => {
+  return (
+    <section className="py-20 px-24 container relative">
+      <div className={cn(styles.gradientBg)}></div>
+      <div className="flex items-center justify-around my-8 ">
+        <div className="title">
+          <h2 className="text-2xl md:text-5xl font-bold font-rubik">
+            <span className="text-white">Hire anyone</span>, <br />
+            <span className="text-gray-400">anywhere</span>
+          </h2>
+        </div>
+        <div className="flex items-start flex-col gap-4">
+          <span className="text-sm font-inter leading-5">
+            Payroll benefits and complains for remote teams, in one secure
+            platform. <br /> Easily onboard and pay employees and contraction
+            worldwide.
+          </span>
+          <Button className="rounded-full px-8">Get Started</Button>
+        </div>
+      </div>
+      <div className="mt-14">
+        <Image
+          src="/images/hero.jpg"
+          alt="hero image"
+          width="2000"
+          height="100"
+          className="rounded-sm h-[500px]"
+        />
+      </div>
+    </section>
+  );
+};
+export default Hero;
---
+++ components/shared/nav-bar.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <nav className="p-4 container">
+      <div className="flex justify-between items-center gap-8">
+        <div className="logo">
+          <Image src="/svg/logo.svg" alt="logo" width={50} height={50} />
+        </div>
+        <div className="navbar-menu hidden md:block bg-transparent ring-1 ring-secondary shadow-sm shadow-primary rounded-full px-8 py-1.5">
+          <ul className="flex justify-between items-center gap-8">
+            <li className="cursor-pointer">Home</li>
+            <li className="cursor-pointer">About</li>
+            <li className="cursor-pointer">Contact</li>
+            <li className="cursor-pointer">Blog</li>
+          </ul>
+        </div>
+        <div className="bg-slate-700  hidden md:flex rounded-full  items-center inset-2   shadow-[inset_5px_5px_10px_rgba(0,0,0,0.6)] gap-4 py-1.5 px-5">
+          <Link href="/sign-in">Login</Link>
+          <div className="border border-primary h-4 "></div>
+          <Link href="/sign-up">Register</Link>
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
