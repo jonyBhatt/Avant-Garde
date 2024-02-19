cc1da465fb31b0c001f1b837104d985544247152
Jony <jonybhattacharjee886@gmail.com>
2024-02-19 23:49:29 +0600

    fix usercrud

--- lib/actions/user/user-crud-action.ts
+++ lib/actions/user/user-crud-action.ts
@@ -1,87 +1,87 @@
-import { use } from "react";
-("use server");
-import prisma from "@/lib/prisma";
-import { handleError } from "@/lib/utils";
-import { CreateUserProps, UpdateUserProps } from "@/utils/types";
+// import { use } from "react";
+// ("use server");
+// import prisma from "@/lib/prisma";
+// import { handleError } from "@/lib/utils";
+// import { CreateUserProps, UpdateUserProps } from "@/utils/types";
 
-export async function createUser(user: CreateUserProps) {
-  try {
-    const newUser = await prisma.user.create({
-      data: {
-        clerkId: user.clerkId,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        email: user.email,
-        photo: user.photo,
-        username: user.username,
-      },
-    });
-    console.log(newUser);
+// export async function createUser(user: CreateUserProps) {
+//   try {
+//     const newUser = await prisma.user.create({
+//       data: {
+//         clerkId: user.clerkId,
+//         firstName: user.firstName,
+//         lastName: user.lastName,
+//         email: user.email,
+//         photo: user.photo,
+//         username: user.username,
+//       },
+//     });
+//     console.log(newUser);
 
-    return {
-      newUser,
-    };
-  } catch (error) {
-    return {
-      error: handleError(error),
-    };
-  }
-}
+//     return {
+//       newUser,
+//     };
+//   } catch (error) {
+//     return {
+//       error: handleError(error),
+//     };
+//   }
+// }
 
-/**
- * * Read user by id
- */
+// /**
+//  * * Read user by id
+//  */
 
-export async function getUserById(userId: string) {
-  try {
-    const user = await prisma.user.findUnique({
-      where: {
-        clerkId: userId,
-      },
-    });
-    return {
-      user,
-    };
-  } catch (error) {
-    return {
-      error: handleError(error),
-    };
-  }
-}
+// export async function getUserById(userId: string) {
+//   try {
+//     const user = await prisma.user.findUnique({
+//       where: {
+//         clerkId: userId,
+//       },
+//     });
+//     return {
+//       user,
+//     };
+//   } catch (error) {
+//     return {
+//       error: handleError(error),
+//     };
+//   }
+// }
 
-/**
- * * Update user
- */
+// /**
+//  * * Update user
+//  */
 
-export async function updateUser(clerkId: string, user: UpdateUserProps) {
-  try {
-    await prisma.user.update({
-      where: {
-        clerkId,
-      },
-      data: user,
-    });
-  } catch (error) {
-    return {
-      error: handleError(error),
-    };
-  }
-}
+// export async function updateUser(clerkId: string, user: UpdateUserProps) {
+//   try {
+//     await prisma.user.update({
+//       where: {
+//         clerkId,
+//       },
+//       data: user,
+//     });
+//   } catch (error) {
+//     return {
+//       error: handleError(error),
+//     };
+//   }
+// }
 
-/**
- * * Delete user
- */
+// /**
+//  * * Delete user
+//  */
 
-export async function deleteUser(clerkId: string) {
-  try {
-    await prisma.user.delete({
-      where: {
-        clerkId,
-      },
-    });
-  } catch (error) {
-    return {
-      error: handleError(error),
-    };
-  }
-}
+// export async function deleteUser(clerkId: string) {
+//   try {
+//     await prisma.user.delete({
+//       where: {
+//         clerkId,
+//       },
+//     });
+//   } catch (error) {
+//     return {
+//       error: handleError(error),
+//     };
+//   }
+// }
