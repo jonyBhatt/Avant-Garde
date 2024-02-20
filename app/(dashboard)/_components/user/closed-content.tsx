44261135dbb12db6dbb18a1e62696b8dafdb0023
Jony <jonybhattacharjee886@gmail.com>
2024-02-20 19:11:28 +0600

    fix tab contents

---
+++ app/(dashboard)/_components/user/closed-content.tsx
@@ -0,0 +1,52 @@
+import { TabsContent } from "@/components/ui/tabs";
+import Image from "next/image";
+
+const ClosedContent = () => {
+  return (
+    <>
+      <TabsContent
+        value="closed"
+        className="my-16 mx-4 p-4 rounded-[8px] bg-muted border border-muted-foreground flex gap-8   items-center"
+      >
+        <div>
+          <Image
+            src="/images/user.jpg"
+            alt="user profile"
+            width={60}
+            height={60}
+            className="rounded-full"
+          />
+        </div>
+        <div className="flex flex-col md:flex-row md:justify-between w-52  items-start xs:gap-4 md:w-full">
+          <div className="flex flex-col gap-1">
+            <h3 className="line-clamp-1 font-bold font-rubik">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio
+              atque aliquam dolore beatae quasi, culpa, quaerat voluptatibus
+              recusandae nesciunt voluptatem omnis rerum commodi totam tempore
+              dignissimos ipsum, a debitis eius!
+            </h3>
+            <div className="flex items-center gap-4">
+              <div className="pl-4 md:px-4 md:py-1 text-xs  bg-dark-pastel-blue">
+                <span className="text-center">Long Term</span>
+              </div>
+              <div className="flex items-center gap-1.5 line-clamp-1">
+                <span>React</span>
+                <span>React</span>
+                <span>React</span>
+                <span>React</span>
+              </div>
+            </div>
+          </div>
+          <div>
+            <p className="font-bold">
+              $15/<span className="font-normal">15 mins</span>
+            </p>
+          </div>
+          <p>2/20/2024</p>
+        </div>
+      </TabsContent>
+    </>
+  );
+};
+
+export default ClosedContent;
--- app/(dashboard)/user-dashboard/post/page.tsx
+++ app/(dashboard)/user-dashboard/post/page.tsx
@@ -1,5 +1,7 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Image from "next/image";
+import OpenContent from "../../_components/user/open-content";
+import ClosedContent from "../../_components/user/closed-content";
 
 const Post = () => {
   return (
@@ -9,89 +11,9 @@ const Post = () => {
           <TabsTrigger value="open">Open</TabsTrigger>
           <TabsTrigger value="closed">Closed</TabsTrigger>
         </TabsList>
-        <TabsContent
-          value="open"
-          className="my-16 mx-4 p-4 rounded-[8px] bg-muted border border-muted-foreground flex gap-8   items-center"
-        >
-          <div>
-            <Image
-              src="/images/user.jpg"
-              alt="user profile"
-              width={60}
-              height={60}
-              className="rounded-full"
-            />
-          </div>
-          <div className="flex flex-col md:flex-row md:justify-between w-52  items-start xs:gap-4 md:w-full">
-            <div className="flex flex-col gap-1">
-              <h3 className="line-clamp-1 font-bold font-rubik">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio
-                atque aliquam dolore beatae quasi, culpa, quaerat voluptatibus
-                recusandae nesciunt voluptatem omnis rerum commodi totam tempore
-                dignissimos ipsum, a debitis eius!
-              </h3>
-              <div className="flex items-center gap-4">
-                <div className="pl-4 md:px-4 md:py-1 text-xs  bg-dark-pastel-blue">
-                  <span className="text-center">Long Term</span>
-                </div>
-                <div className="flex items-center gap-1.5 line-clamp-1">
-                  <span>React</span>
-                  <span>React</span>
-                  <span>React</span>
-                  <span>React</span>
-                </div>
-              </div>
-            </div>
-            <div>
-              <p className="font-bold">
-                $15/<span className="font-normal">15 mins</span>
-              </p>
-            </div>
-            <p>2/20/2024</p>
-          </div>
-        </TabsContent>
 
-        <TabsContent
-          value="closed"
-          className="my-16 mx-4 p-4 rounded-[8px] bg-muted border border-muted-foreground flex gap-8   items-center"
-        >
-          <div>
-            <Image
-              src="/images/user.jpg"
-              alt="user profile"
-              width={60}
-              height={60}
-              className="rounded-full"
-            />
-          </div>
-          <div className="flex flex-col md:flex-row md:justify-between w-52  items-start xs:gap-4 md:w-full">
-            <div className="flex flex-col gap-1">
-              <h3 className="line-clamp-1 font-bold font-rubik">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odio
-                atque aliquam dolore beatae quasi, culpa, quaerat voluptatibus
-                recusandae nesciunt voluptatem omnis rerum commodi totam tempore
-                dignissimos ipsum, a debitis eius!
-              </h3>
-              <div className="flex items-center gap-4">
-                <div className="pl-4 md:px-4 md:py-1 text-xs  bg-dark-pastel-blue">
-                  <span className="text-center">Long Term</span>
-                </div>
-                <div className="flex items-center gap-1.5 line-clamp-1">
-                  <span>React</span>
-                  <span>React</span>
-                  <span>React</span>
-                  <span>React</span>
-                </div>
-              </div>
-            </div>
-            <div>
-              <p className="font-bold">
-                $15/<span className="font-normal">15 mins</span>
-              </p>
-            </div>
-            <p>2/20/2024</p>
-          </div>
-        </TabsContent>
+        <OpenContent />
+        <ClosedContent />
       </Tabs>
     </div>
   );
