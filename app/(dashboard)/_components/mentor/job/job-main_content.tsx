4155f09a170d0d276eceacc25f4422bff42ec163
Jony <jonybhattacharjee886@gmail.com>
2024-03-05 17:55:07 +0600

    added icons

--- app/(dashboard)/_components/mentor/job/job-main_content.tsx
+++ app/(dashboard)/_components/mentor/job/job-main_content.tsx
@@ -1,4 +1,14 @@
 const JobMainContent = () => {
-  return <div>JobMainContent</div>;
+  return (
+    <div className="flex flex-col items-start gap-6 pr-2">
+      <div className="flex justify-between items-center w-full">
+        <h3 className="text-lg">
+          Showing <b>100</b> Jobs Available
+        </h3>
+        <span>Sort By</span>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4"></div>
+    </div>
+  );
 };
 export default JobMainContent;
