2ad670a902604c86d6d977e51a5ab769a833f774
Jony <jonybhattacharjee886@gmail.com>
2024-04-25 01:37:04 +0600

    style: Mentor Forem Shop

---
+++ app/(dashboard)/mentor-dashboard/forem-store/_components/update-product.tsx
@@ -0,0 +1,220 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { CiCircleAlert } from "react-icons/ci";
+
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+
+import UploadButton from "@/lib/upload-button";
+
+import { ForemData } from "@/utils/data/forem-data";
+
+const formSchema = z.object({
+  title: z.string().min(2, {
+    message: "title must be at least 2 characters.",
+  }),
+  description: z.string().min(1, { message: "Description Required" }),
+  price: z.string().min(1, { message: "Price required" }),
+  size: z.enum(["S", "M", "L", "XL", "XXL", "XXXL"], {
+    required_error: "You need to select a size.",
+  }),
+  category: z.string(),
+  photo: z.string().optional(),
+});
+
+export const UpdateShopProduct = ({ id }: { id: number }) => {
+  const product = ForemData.find((item) => item.id === id);
+  console.log(product);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: product?.name || "",
+      category: product?.cat || "",
+      description: product?.desc || "",
+      photo: product?.url || "",
+      price: product?.price || "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+  return (
+    <ScrollArea className="h-[500px]  ">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 px-7 pb-4"
+        >
+          <div className="grid grid-cols-1 gap-6">
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="product title" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      rows={15}
+                      placeholder="shadcn"
+                      {...field}
+                      className="bg-transparent border-2 border-muted-foreground rounded-[8px]
+                        ring-offset-0 focus-visible:outline-none focus-visible:ring-0  focus-visible:ring-offset-0"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />{" "}
+            <FormField
+              control={form.control}
+              name="price"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Price</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="&#x9F3;1000"
+                      {...field}
+                      className="bg-transparent border-2 border-muted-foreground rounded-[8px]
+                        ring-offset-0 focus-visible:outline-none focus-visible:ring-0  focus-visible:ring-offset-0"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="size"
+              render={({ field }) => (
+                <FormItem className="space-y-3">
+                  <FormLabel>Size</FormLabel>
+                  <FormControl>
+                    <RadioGroup
+                      onValueChange={field.onChange}
+                      defaultValue={field.value}
+                      className="flex lg:flex-row xs:flex-col space-y-1 gap-4"
+                    >
+                      <FormItem className="flex items-center space-x-1 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="S" />
+                        </FormControl>
+                        <FormLabel className="font-normal">Small</FormLabel>
+                      </FormItem>
+                      <FormItem className="flex items-center space-x-1 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="M" />
+                        </FormControl>
+                        <FormLabel className="font-normal">Medium</FormLabel>
+                      </FormItem>
+                      <FormItem className="flex items-center space-x-1 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="L" />
+                        </FormControl>
+                        <FormLabel className="font-normal">Large</FormLabel>
+                      </FormItem>
+                      <FormItem className="flex items-center space-x-1 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="XL" />
+                        </FormControl>
+                        <FormLabel className="font-normal">XL</FormLabel>
+                      </FormItem>
+                      <FormItem className="flex items-center space-x-1 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="XXL" />
+                        </FormControl>
+                        <FormLabel className="font-normal">2XL</FormLabel>
+                      </FormItem>
+                      <FormItem className="flex items-center space-x-1 space-y-0">
+                        <FormControl>
+                          <RadioGroupItem value="XXXL" />
+                        </FormControl>
+                        <FormLabel className="font-normal">3XL</FormLabel>
+                      </FormItem>
+                    </RadioGroup>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="category"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Product Category</FormLabel>
+                  <FormControl>
+                    <Input
+                      placeholder="Add a category"
+                      {...field}
+                      className="bg-transparent border-2 border-muted-foreground rounded-[8px]
+                        ring-offset-0 focus-visible:outline-none focus-visible:ring-0  focus-visible:ring-offset-0"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="photo"
+              render={({ field }) => (
+                <FormItem className="mt-8">
+                  <FormLabel>Product Image</FormLabel>
+                  <FormControl>
+                    <UploadButton
+                      endpoint="imageUploader"
+                      onChange={field.onChange}
+                      value={field.value}
+                    />
+                  </FormControl>
+                  <FormDescription className="flex items-center gap-1">
+                    <CiCircleAlert className="w-4 h-4 text-muted-foreground" />
+                    <span>
+                      Pay attention to the quality of the pictures you add
+                    </span>
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+
+          <Button type="submit" size={"lg"} className="w-full rounded">
+            Update
+          </Button>
+        </form>
+      </Form>
+    </ScrollArea>
+  );
+};
--- app/(dashboard)/mentor-dashboard/forem-store/page.tsx
+++ app/(dashboard)/mentor-dashboard/forem-store/page.tsx
@@ -1,7 +1,26 @@
+import { Button } from "@/components/ui/button";
 import { Plus, Store } from "lucide-react";
+import Image from "next/image";
 import Link from "next/link";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { UpdateShopProduct } from "./_components/update-product";
+
 const ForemShop = () => {
+  // function getRandomID() {
+  //   const randomNumber = Math.floor(Math.random() * 10) + 1;
+  //   return  + randomNumber;
+  // }
+  const id = Math.floor(Math.random() * 10) + 1;
+  console.log(id);
+
   return (
     <div className="py-6 relative">
       <div className="flex items-center gap-2 justify-center">
@@ -10,9 +29,114 @@ const ForemShop = () => {
           Forem Shop
         </h2>
       </div>
+
+      {/** Fetch all product which created by login user */}
+      <div className="grid grid-cols-1 md:grid-cols-2 w-full lg:grid-cols-3 gap-4 pr-4 mt-16 pb-16">
+        {/** Card 1 */}
+        <div className="border-2  py-6 px-4 rounded-[8px]">
+          <span className="text-muted-foreground tracking-widest uppercase font-inter font-light text-sm">
+            {/* {item.featuredTitle} */}
+            #MUG
+          </span>
+          <div className="flex flex-col gap-4 mt-4">
+            <Image src={`/forem/e1.jpg`} alt="p1" width={300} height={300} />
+            <h2 className="font-bold font-rubik text-2xl">Mug</h2>
+            <p className="font-inter text-muted-foreground">Tk 2000 BDT</p>
+          </div>
+          <div className="flex items-center gap-4  justify-between">
+            <Dialog>
+              <DialogTrigger asChild>
+                <Button size={"lg"} className="mt-4 w-full  rounded">
+                  Update
+                </Button>
+              </DialogTrigger>
+              <DialogContent className="p-0">
+                <DialogHeader className="p-4">
+                  <DialogTitle>Update Product</DialogTitle>
+                </DialogHeader>
+                <div className="">
+                  <UpdateShopProduct id={id} />
+                </div>
+              </DialogContent>
+            </Dialog>
+            <Button
+              size={"lg"}
+              variant={"destructive"}
+              className="mt-4 w-full  rounded"
+            >
+              Delete
+            </Button>
+          </div>
+        </div>
+
+        {/** Card 2 */}
+        <div className="border-2  py-6 px-4 rounded-[8px]">
+          <span className="text-muted-foreground tracking-widest uppercase font-inter font-light text-sm">
+            {/* {item.featuredTitle} */}
+            #MUG
+          </span>
+          <div className="flex flex-col gap-4 mt-4">
+            <Image src={`/forem/e1.jpg`} alt="p1" width={300} height={300} />
+            <h2 className="font-bold font-rubik text-2xl">Mug</h2>
+            <p className="font-inter text-muted-foreground">Tk 2000 BDT</p>
+          </div>
+          <Button size={"lg"} className="mt-4 w-full rounded">
+            Update
+          </Button>
+        </div>
+
+        {/** Card 3 */}
+        <div className="border-2  py-6 px-4 rounded-[8px]">
+          <span className="text-muted-foreground tracking-widest uppercase font-inter font-light text-sm">
+            {/* {item.featuredTitle} */}
+            #MUG
+          </span>
+          <div className="flex flex-col gap-4 mt-4">
+            <Image src={`/forem/e1.jpg`} alt="p1" width={300} height={300} />
+            <h2 className="font-bold font-rubik text-2xl">Mug</h2>
+            <p className="font-inter text-muted-foreground">Tk 2000 BDT</p>
+          </div>
+          <Button size={"lg"} className="mt-4 w-full rounded">
+            Update
+          </Button>
+        </div>
+
+        {/** Card 4 */}
+        <div className="border-2  py-6 px-4 rounded-[8px]">
+          <span className="text-muted-foreground tracking-widest uppercase font-inter font-light text-sm">
+            {/* {item.featuredTitle} */}
+            #MUG
+          </span>
+          <div className="flex flex-col gap-4 mt-4">
+            <Image src={`/forem/e1.jpg`} alt="p1" width={300} height={300} />
+            <h2 className="font-bold font-rubik text-2xl">Mug</h2>
+            <p className="font-inter text-muted-foreground">Tk 2000 BDT</p>
+          </div>
+          <Button size={"lg"} className="mt-4 w-full rounded">
+            Update
+          </Button>
+        </div>
+
+        {/** Card 5 */}
+        <div className="border-2  py-6 px-4 rounded-[8px]">
+          <span className="text-muted-foreground tracking-widest uppercase font-inter font-light text-sm">
+            {/* {item.featuredTitle} */}
+            #MUG
+          </span>
+          <div className="flex flex-col gap-4 mt-4">
+            <Image src={`/forem/e1.jpg`} alt="p1" width={300} height={300} />
+            <h2 className="font-bold font-rubik text-2xl">Mug</h2>
+            <p className="font-inter text-muted-foreground">Tk 2000 BDT</p>
+          </div>
+          <Button size={"lg"} className="mt-4 w-full rounded">
+            Update
+          </Button>
+        </div>
+      </div>
+
       <Link
         href={"/mentor-dashboard/forem-store/add"}
-        className="absolute bottom-64 bg-secondary w-14 h-14 flex items-center justify-center right-0 mr-4 rounded-full"
+        className="absolute bottom-1 bg-secondary w-14 h-14 flex items-center justify-center right-0 mr-4 rounded-full"
       >
         <Plus className="" />
       </Link>
