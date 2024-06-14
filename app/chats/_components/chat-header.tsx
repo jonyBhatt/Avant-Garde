5c8cc73cbf1cd9e59dadfcc1ce8a0c46ef5bf27d
Jony <jonybhattacharjee886@gmail.com>
2024-06-14 17:56:24 +0600

    Chat header

--- app/chats/_components/chat-header.tsx
+++ app/chats/_components/chat-header.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import {
   Sheet,
   SheetContent,
@@ -6,13 +6,37 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
 import { BiDotsVerticalRounded } from "react-icons/bi";
 import { AddContactsSection } from "./add-contacts/add-contact-sction";
-export default function ChatHeader() {
+import Image from "next/image";
+import { getChatUser } from "@/lib/actions/chat/get-chat-current-user";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+export default async function ChatHeader() {
+  const user = await getChatUser();
+  if (!user) return null;
   return (
     <div className="p-4 bg-muted w-full">
       <div className="flex justify-between items-center">
-        <div>User Head</div>
+        <div className="flex items-center gap-2">
+          <Avatar>
+            <AvatarImage src={user.currentUserPrisma.photo} />
+            <AvatarFallback>
+              {user.currentUserPrisma.firstName.slice(0, 1)}
+            </AvatarFallback>
+          </Avatar>
+
+          <div className="flex flex-col gap-1">
+            <h3 className="font-rubik font-bold text-base">
+              {user.currentUserPrisma.firstName}
+            </h3>
+            <span className="font-inter font-light text-sm text-muted-foreground">
+              Chats Room
+            </span>
+          </div>
+        </div>
         {/** Mobile */}
         <div className="md:hidden sm:block">
           <Sheet>
@@ -48,7 +72,30 @@ export default function ChatHeader() {
         </div>
         <div className="hidden md:flex items-center gap-4">
           <AddContactsSection />
-          <span>Dashboard Link</span>
+          {user.currentUserPrisma.role === "STUDENT" ? (
+            <>
+              <Link
+                href={"/user-dashboard"}
+                className={buttonVariants({
+                  size: "lg",
+                })}
+              >
+                Dashboard
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link
+                href={"/mentor-dashboard"}
+                className={buttonVariants({
+                  size: "lg",
+                  className: "rounded-[20px]",
+                })}
+              >
+                Dashboard
+              </Link>
+            </>
+          )}
         </div>
       </div>
     </div>
