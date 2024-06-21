09ed56acb3cb9fcd042ce8eb78fe3772f88f850a
Jony <jonybhattacharjee886@gmail.com>
2024-06-22 00:07:06 +0600

    Seen Info  fix

--- app/chats/[id]/_components/meeage-box.tsx
+++ app/chats/[id]/_components/meeage-box.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 import { FullMessageType } from "@/utils/types";
 import { User } from "@prisma/client";
 import clsx from "clsx";
 import Image from "next/image";
 import { useMemo } from "react";
+import { format } from "date-fns";
 
 interface MessageBoxProps {
   messages: FullMessageType;
@@ -19,13 +21,20 @@ export const MessageBox = ({
   // console.log(currentUser?.id);
 
   // console.log(ownChat);
+  console.log(messages.seen);
 
-  const seenList = (messages.seen || [])
-    .filter((user) => user.id !== messages.sender.id)
-    .join(", ");
-  const seenInfo = useMemo(() => {
-    seenList.length > 0 ? "/svg/seen.svg" : "/svg/Sent.svg";
-  }, [seenList]);
+  const seenList = (messages.seen || []).filter(
+    (user) => messages.seen && user.id !== messages.sender.id
+  );
+
+  // console.log(seenList.length);
+
+  let seenInfo: string;
+  if (seenList.length > 0) {
+    seenInfo = "/svg/seen.svg";
+  } else {
+    seenInfo = "/svg/sent.svg";
+  }
 
   return (
     <div
@@ -44,10 +53,10 @@ export const MessageBox = ({
         <div
           className={clsx(
             "text-sm w-fit overflow-hidden",
-            ownChat ? "bg-[#d1f4cc] text-black" : "bg-gray-100 text-black",
+            ownChat ? "bg-muted " : "bg-gray-100 text-black",
             messages.image
               ? "rounded-[3px]"
-              : "rounded-[7px] py-2 px-3 shadow-lg shadow-gray-300 "
+              : "rounded-[7px] py-2 px-3 shadow-sm shadow-purple-500 "
           )}
         >
           {messages.image ? (
@@ -60,7 +69,21 @@ export const MessageBox = ({
             />
           ) : (
             <div className="flex flex-col relative max-w-[600px]">
-              <p className="mb-2 break-words mr-20">{messages.body}</p>
+              <p className="mb-2 break-words mr-20 font-inter">
+                {messages.body}
+              </p>
+              <div className="flex justify-end items-center absolute bottom-0 right-0">
+                <p className="text-[11px h-[15px] text-gray-500 font-rubik">
+                  {format(new Date(messages.createdAt), "p")}
+                </p>
+                {ownChat && (
+                  <img
+                    src={seenInfo}
+                    alt="seen"
+                    className="mr-[2px] ml-[2px] w-[16px] h-[11px]"
+                  />
+                )}
+              </div>
             </div>
           )}
         </div>
